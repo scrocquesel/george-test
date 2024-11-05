b2483f865d7af8c108c5e1dc2c3bd2921b3f9643
George Gastaldi <gegastaldi@gmail.com>
2024-11-05 18:58:20 -0300

    Update renovate-config.js

--- renovate-config.js
+++ renovate-config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  allowedPostUpgradeCommands: ['git reset --hard', 'quarkus update -Dstream=3.16']
+  "allowedPostUpgradeCommands": ['git reset --hard', 'quarkus update -Dstream=3.16'],
+  "autodiscover": true,
+  "autodiscoverFilter": ["quarkiverse/george-test"]
 };
