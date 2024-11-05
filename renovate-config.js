4205a9e1818ac6af9512dc216eab8a443b94165e
George Gastaldi <gegastaldi@gmail.com>
2024-11-05 19:24:02 -0300

    Wrapper

--- renovate-config.js
+++ renovate-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "autodiscover": true,
   "autodiscoverFilter": ["quarkiverse/george-test"],
-  "allowedPostUpgradeCommands": ['^mvn io.quarkus.platform:quarkus-maven-plugin:3.16.1:update -N$']
+  "allowedPostUpgradeCommands": ['^./mvnw io.quarkus.platform:quarkus-maven-plugin:3.16.1:update -N$']
 
 };
