6a968ecb04791a7943629575b4fcaadc076ffbf9
Sébastien Crocquesel <88554524+scrocquesel@users.noreply.github.com>
2024-11-08 21:43:07 +0100

    test

--- renovate-config.js
+++ renovate-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  "forkProcessing": "enabled", // Repository is a fork and not manually configured - skipping - did you want to run with --fork-processing=enabled?
   "autodiscover": true,
   "autodiscoverFilter": ["scrocquesel/george-test"],
   "allowedPostUpgradeCommands": ['^.*$'],
